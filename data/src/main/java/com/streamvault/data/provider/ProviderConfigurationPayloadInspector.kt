7ba3178b963af984481e6185f44cbc05f3acf000
vallembreuse <59437520+vallembreuse@users.noreply.github.com>
2026-08-30 12:09:14 +0200

    chore: remove superseded provider payload inspector

--- data/src/main/java/com/streamvault/data/provider/ProviderConfigurationPayloadInspector.kt
+++
@@ -1,6 +0,0 @@
-package com.streamvault.data.provider
-
-import com.google.gson.Gson
-
-internal fun Gson.providerPayloadHasEmbeddedType(payload: String): Boolean =
-    fromJson(payload, Map::class.java)?.containsKey("type") == true
